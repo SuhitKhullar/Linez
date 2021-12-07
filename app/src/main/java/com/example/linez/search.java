3d5192a0b09d4163e13cbbc57e99543579cd527a
Andrew <ajcohen9@wisc.edu>
2021-12-07 13:59:26 -0600

    added dummy locations

--- app/src/main/java/com/example/linez/search.java
+++ app/src/main/java/com/example/linez/search.java
@@ -1,12 +1,18 @@
 package com.example.linez;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+
+import java.util.ArrayList;
 
 public class search extends AppCompatActivity {
 
@@ -18,7 +24,12 @@ public class search extends AppCompatActivity {
         setContentView(R.layout.activity_search);
         mAuth = FirebaseAuth.getInstance();
 
-        generateLocations();
+        SearchView searchView = findViewById(R.id.searchView);
+        ListView listView = findViewById(R.id.list);
+
+
+
+        populateSearchView(generateLocations(), listView);
     }
 
     @Override
@@ -31,10 +42,27 @@ public class search extends AppCompatActivity {
 
     }
 
-    public void generateLocations(){
-        LinezLocation chipotle = new LinezLocation(43.07525196698477, -89.39651031646711, "Chipotle", 30);
-        LinezLocation nick = new LinezLocation(43.0708749120671, -89.39853524433501, "Nicholas Recreation Center", 30);
-        LinezLocation strada = new LinezLocation(43.07633573329389, -89.39956247021655, "Strada", 30);
-        LinezLocation gingerRoot = new LinezLocation(43.0721235311657, -89.40792344433494, "Ginger Root", 30);
+    public ArrayList<LinezLocation> generateLocations(){
+
+        ArrayList<LinezLocation> places = new ArrayList<LinezLocation>();
+
+        places.add(new LinezLocation(43.07525196698477, -89.39651031646711, "Chipotle", 30));
+        places.add(new LinezLocation(43.0708749120671, -89.39853524433501, "Nicholas Recreation Center", 30));
+        places.add(new LinezLocation(43.07633573329389, -89.39956247021655, "Strada", 30));
+        places.add(new LinezLocation(43.0721235311657, -89.40792344433494, "Ginger Root", 30));
+
+        return places;
+    }
+
+    public void populateSearchView(ArrayList<LinezLocation> places, ListView listView){
+        ArrayList<String> names = new ArrayList<String>();
+        for(LinezLocation location : places){
+            names.add(location.getName());
+        }
+
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>
+                (this, android.R.layout.simple_list_item_1, names);
+        listView.setAdapter(arrayAdapter);
+
     }
 }
